826a8e42ac6387edf916b9930502b3fa8b02c750
awansch1 <awansch@student.tgm.ac.at>
2023-09-24 22:37:58 +0200

    add WortBildPaar class

---
+++ src/main/java/at/ac/tgm/awansch/WortBildPaar.java
@@ -0,0 +1,27 @@
+package at.ac.tgm.awansch;
+
+public class WortBildPaar {
+    private String wort;
+    private String url;
+
+    WortBildPaar(String wort, String url) {
+        this.wort = wort;
+        this.url = url;
+    }
+
+    public String getWort() {
+        return wort;
+    }
+
+    public void setWort(String wort) {
+        this.wort = wort;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
