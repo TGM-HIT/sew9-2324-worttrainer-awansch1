dfbb04da9a1b5c7b7c1016f7d098292dccc1876d
Alexander Wansch <awansch@student.tgm.ac.at>
2023-10-04 13:38:41 +0200

    add Statistik class

--- src/main/java/at/ac/tgm/awansch/Statistik.java
+++ src/main/java/at/ac/tgm/awansch/Statistik.java
@@ -1,4 +1,38 @@
 package at.ac.tgm.awansch;
 
 public class Statistik {
+    private int versuche;
+    private int korrekteVersuche;
+    private int falscheVersuche;
+
+    public Statistik() {
+        this.versuche = 0;
+        this.korrekteVersuche = 0;
+        this.falscheVersuche = 0;
+    }
+
+    public void versucheErhoehen() {
+        this.versuche++;
+    }
+
+    public int getVersuche() {
+        return this.versuche;
+    }
+
+
+    public void korrekteVersucheErhoehen() {
+        this.korrekteVersuche++;
+    }
+
+    public int getKorrekteVersuche() {
+        return this.korrekteVersuche;
+    }
+
+    public void falscheVersucheErhoehen() {
+        this.falscheVersuche++;
+    }
+
+    public int getFalscheVersuche() {
+        return this.falscheVersuche;
+    }
 }
